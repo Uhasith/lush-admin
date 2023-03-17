e849cbe0871a3289368d694bde28f49e38985243
Hasith Udayanga <78094049+Uhasith@users.noreply.github.com>
2023-03-17 08:37:40 +0530

    fix: page changing issue when updating a product

--- client/src/content/Sections/Products/ProductTable.tsx
+++ client/src/content/Sections/Products/ProductTable.tsx
@@ -1,4 +1,4 @@
-import { FC, ChangeEvent, useState } from 'react';
+import { FC, ChangeEvent, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import leadingZeroes from 'leading-zeroes';
 import {
@@ -53,6 +53,17 @@ const ProductTable: FC<ProductTableProps> = ({ products }) => {
 
   const theme = useTheme();
 
+  useEffect(() => {
+    const savedPage = localStorage.getItem('page');
+    if (savedPage) {
+      setPage(parseInt(savedPage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.removeItem('page');
+  }, []);
+
   const applyPagination = (
     _products: any,
     page: number,
@@ -63,6 +74,7 @@ const ProductTable: FC<ProductTableProps> = ({ products }) => {
 
   const handlePageChange = (event: any, newPage: number): void => {
     setPage(newPage);
+    localStorage.setItem('page', newPage.toString());
   };
 
   const handleLimitChange = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -111,7 +123,6 @@ const ProductTable: FC<ProductTableProps> = ({ products }) => {
       data: Object.assign({ hasPromotion: !hasPromotion }) 
     }));
   };
-
 
   
 
